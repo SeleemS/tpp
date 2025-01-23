db54ccc060ebdf310e0486e6996dc14cb4c004f0
Seleem <127633573+SeleemS@users.noreply.github.com>
2025-01-24 01:42:21 +0200

    update notion link

--- pages/careers.tsx
+++ pages/careers.tsx
@@ -3,7 +3,7 @@ import { GetServerSideProps } from 'next';
 export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
     redirect: {
-      destination: 'https://thepotcastproductions.notion.site/Hiring-3a9f64f8c693463ab4557287dd0d85b8?pvs=4',
+      destination: 'https://tppnetwork.notion.site/Job-Board-The-Potcast-Productions-3a9f64f8c693463ab4557287dd0d85b8?pvs=4',
       permanent: true, // Set to true if this is a permanent redirect
     },
   };
